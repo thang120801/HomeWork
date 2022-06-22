4485d78559a48692898c8f1203d5e50947ffaa3a
your name <tran65092@gmail.com>
2022-06-22 16:07:31 +0700

    first commit

---
+++ Bai 1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    long n; //gia tri n
+    long result=0; //result la tong cua phuong trinh
+
+    //NHAP VAO GIA TRI CUA n
+    printf("S=1+2+3+4...+n  ---  Nhap vao gia tri cua n:");
+    scanf("%ld",&n);
+
+    //TINH TONG
+    for(long count=1;count<=n;count++)   //count chay tu 1-->6
+    {
+        result = result + count;
+    }
+    printf("Phuong trinh S=1+2+3+4+...+n  ---  voi n = %ld co ket qua la s = %ld",n,result);
+    return 0;
+}
+
+
---
+++ Bai 2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>   //thu vien toan hoc
+
+int main()
+{
+    //KHAI BAO
+    long result=0;
+    long n; //gioi han cua count
+    long count;     //count chay tu 1 --> n
+
+    //LAY GIA TRI n
+    printf("s=1^2 + 2^2 + 3^2 + ... n^2 --- nhap vao gia tri cua n:");
+    scanf("%ld",&n);
+
+    //TINH TONG
+    for(count=1;count<=n;count++)
+    {
+        result=result+pow(count,2);
+    }
+    printf("ket qua s=1^2 + 2^2 + 3^2 + ... n^2 --- voi n=%ld la s=%ld",n,result);
+    return 0;
+}
