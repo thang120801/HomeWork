211b945522c0ce8399e7761bbf1295d6779e6a89
your name <tran65092@gmail.com>
2022-07-06 21:49:21 +0700

    vua lam

---
+++ Bai 51/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//KHAI BÁO   
+    int input;
+    int soDu;
+    int result=0;
+ 
+//NHẬP VÀO INPUT
+    while(input<=0)
+    {
+        printf("nhap vao so nguyen duong ban muon:");
+        scanf("%d",&input);
+    }
+
+//TÍNH TOÁN
+    while(input%10!=input)
+    {
+        soDu=input%10;
+        if(soDu>result)
+        {
+            result=soDu;
+        }
+        input=input/10;     //vứt đi số sau cùng vừa so sánh xong
+
+    }
+    //khi đến số cuối cùng thì bị thoát loop nên bổ sung thêm 2 instruction phía trên ở dưới này
+    soDu=input%10;
+    if(soDu>result)
+    {
+        result=soDu;
+    }
+    //khi so sánh hết các số rồi thì in ra số lớn nhất
+    printf("chu so lon nhat la %d",result);
+    return 0;
+
+}
+
