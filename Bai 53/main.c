6b44c7e984709633a8d21356f800ec408089f979
Cao Thang <tran65092@gmail.com>
2022-07-08 10:26:30 +0700

    vua xong

--- Bai 53/main.c
+++ Bai 53/main.c
@@ -4,19 +4,32 @@
 int main()
 {
     //KHAI BAO
-    int input=1;
-    int count;      //dem so luon so nguyen lon nhat as 
-    
-    if (input!=1)
+    int input=0;
+    int soDu;
+
+    //LAY GIA TRI INPUT
+    while (input<=0)
     {
-        printf("input khac 1 \n");
+        printf("Nhap vao gia tri input:");
+        scanf("%d",&input);
     }
-    else
+
+    //TINH TOAN INPUT CO PHAI SO TOAN SO LE HAY K
+    while(input%10!=input)   //khi nao den chu so cuoi cung thi thoat loop
     {
-        printf("input bang 1 \n");
+        
+        if(input%2==0)    //neu trong input co 1 so chan thi thong bao ket qua va thoat loop
+        {
+            printf("So nay khong phai so toan chu so le \n");
+            break;
+        }
+        input=input/10; //vứt 1 số sau cùng vừa xét để xét tiếp số tiếp theo
     }
+    if(input%2!=0)  //vì loop trên còn 1 chữ số thì sẽ out loop, nên ở đây xét thêm số còn lại cuối cùng
+    {
+        printf("So nay la so co tat ca chu so la so le \n");    //sau khi xet het cac chu so khong co so nao la so chan thi in man hinh ket qua
+    }
+   
     return 0;
-    
-    
 
 }
