5fdec03168042ac1d6d0bc8e73e1604c89b90d16
Cao Thang <tran65092@gmail.com>
2022-07-08 09:10:12 +0700

    vuaxong

---
+++ Bai 53/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //KHAI BAO
+    int input=1;
+    int count;      //dem so luon so nguyen lon nhat as 
+    
+    if (input!=1)
+    {
+        printf("input khac 1 \n");
+    }
+    else
+    {
+        printf("input bang 1 \n");
+    }
+    return 0;
+    
+    
+
+}
